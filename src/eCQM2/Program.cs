41fce9d694484ceb7a4a82356d6eda4dfc79dc88
Bhawani Parajuli <parajulibhawani@gmail.com>
2017-02-11 12:26:01 -0800

    create new crawler program.

---
+++ src/eCQM2/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+
+namespace eCQM2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Crawler();
+            Console.ReadLine();
+
+        }
+        private static async void Crawler()
+        {
+            var url = "https://ecqi.healthit.gov/ep/ecqms-2017-performance-period";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var tds = htmlDocument.DocumentNode.Descendants("td")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("views-field views-field-title active")).ToList();
+
+            foreach (var td in tds)
+            {
+                var uri = "https://ecqi.healthit.gov" + td.Descendants("a")?.FirstOrDefault()?.ChildAttributes("href")?.FirstOrDefault().Value;
+                await Task.Delay(5000);
+                var newHttpClient = new HttpClient();
+                var newHtml = await newHttpClient.GetStringAsync(uri);
+                var newHtmlDocument = new HtmlDocument();
+                newHtmlDocument.LoadHtml(newHtml);
+                var htmlFiles = newHtmlDocument.DocumentNode.Descendants("span")
+                    .Where(n => n.GetAttributeValue("class", "")
+                    .Equals("file")).ToArray();
+
+                for (var i = 1; i < 3; i++)
+                {
+                    var htmlUrl = htmlFiles[2].Descendants("a")?.ElementAt(0).ChildAttributes("href")?.FirstOrDefault().Value;
+                    await Task.Delay(2000);
+                    var htmlHttpClient = new HttpClient();
+                    var newHtml2 = await htmlHttpClient.GetStringAsync(htmlUrl);
+                    
+                    var xmlDoc = XDocument.Parse(newHtml2);
+
+                    //measure details node
+                    var title = xmlDoc.Root?.XPathSelectElement("/measure/measureDetails/title")?.Value;
+                    var eMeasureId = xmlDoc.Root?.XPathSelectElement("/measure/measureDetails/emeasureid")?.Value;
+                    var version = xmlDoc.Root?.XPathSelectElement("/measure/measureDetails/version")?.Value;
+                    var guidance = xmlDoc.Root?.XPathSelectElement("/measure/measureDetails/guidance")?.Value;
+                    var clinicalRecommendation = xmlDoc.Root?.XPathSelectElement("/measure/measureDetails/recommendations")?.Value;
+                    var rationale = xmlDoc.Root?.XPathSelectElement("/measure/measureDetails/rationale")?.Value;
+                    var references = xmlDoc.Root?.XPathSelectElements("/measure/measureDetails/references/reference")?.ToList();
+
+                    //measure grouping
+                    var measureGroupingNodes = xmlDoc.Root?.XPathSelectElements("/measure/measureGrouping/group/clause")?.ToList();
+
+                    //element lookup
+                    var elementLookUpList = xmlDoc.Root?.XPathSelectElements("/measure/elementLookUp/qdm")?.ToList();
+                }
+            }
+        }
+    }
+}
---
+++ src/eCQM2/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("eCQM2")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("620e6053-ed24-4b15-9b2d-a2031c1082c2")]
